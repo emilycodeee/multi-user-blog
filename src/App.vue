ab340d47856db1eca8911239e0e48f17e5a7f05d
Emily Chen <emilycodeee@gmail.com>
2022-01-21 15:53:09 +0800

    init : init firebase.js and build navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
+    <router-link to="/">Home</router-link>
   </div>
-  <router-view/>
+  <Navigation/>
+  <router-view />
 </template>
+<script>
+import Navigation from './components/Navigation.vue'
+export default {
+  name: 'App',
+  components: { Navigation }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+</script>
+<style lang="scss" >
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #fff;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  setup () {
+    console.log('process.env.VUE_APP_apiKey')
+    console.log(process.env.VUE_APP_API_KEY)
+    console.log(process.env.VUE_APP_SOMEKEY)
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
